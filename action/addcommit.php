cfdeb05b45f2347b95172577137657be2b75bd19
Richard Mahn <richmahn@users.noreply.github.com>
2016-04-08 11:34:07 -0600

    Change so if there are any conflicts, it uses "ours" since we ave saving the page

--- action/addcommit.php
+++ action/addcommit.php
@@ -99,7 +99,7 @@ class action_plugin_gitcommit_addcommit extends DokuWiki_Action_Plugin {
             }            
 
             $output = array();
-            exec("/usr/bin/git pull --no-edit origin master", $output, $rc);
+            exec("/usr/bin/git pull -X ours --no-edit origin master", $output, $rc);
             if ($debug) {
                 msg("git pull output [" . $rc . "] <pre>" . print_r($output, TRUE) . "</pre>");
             }
